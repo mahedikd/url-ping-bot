3a40c2cc5e483d137bcc429069181805a3b7b369
mahedikd <contact@solodevr.com>
2025-07-16 19:26:32 +0600

    fix: typo

--- utils/config.py
+++ utils/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
-REMOVE_INTERVAL_HOURS = int(os.getenv("INTERVAL_HOURS") or 0)
+REMOVE_INTERVAL_HOURS = int(os.getenv("REMOVE_INTERVAL_HOURS") or 0)
 CHAT_ID = int(os.getenv("CHAT_ID") or 0)
